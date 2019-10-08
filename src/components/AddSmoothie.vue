e23e30caba2490dd2b7b2b5b369a1b53d5f4c436
Christos Giannouris <chris1giann1@gmail.com>
2019-10-08 11:58:54 +0200

    Addsmoothie component

--- src/components/AddSmoothie.vue
+++ src/components/AddSmoothie.vue
@@ -1,16 +1,17 @@
 <template>
     <div class="add-smoothie container">
-        <h2 class="center-align indigo-text">Add New Smoothie</h2>
-        <form>
+        <h2 class="center-align indigo-text">Add New Smoothie Recipe</h2>
+        <form @submit.prevent="AddSmoothie">
             <div class="field title">
                 <label for="title">Smoothie Title:</label>
                 <input type="text" name="title" v-model="title" />
             </div>
             <div class="field add-ingredient">
-                <label for="add-ingredient">Add an igredient:</label>
-                <input type="text" name="add-ingredient" v-model="title" />
+                <label for="add-ingredient">Add an ingredient:</label>
+                <input @keydown.tab="addIng" v-model="another" type="text" name="add-ingredient" />
             </div>
             <div class="field center-align">
+                <p v-if="feedback" class="red-text">{{feedback}}</p>
                 <button class="btn pink">Add Smoothie</button>
             </div>
         </form>
@@ -22,11 +23,40 @@ export default {
     name: "AddSmoothie",
     data() {
         return {
-            title: null
+            title: null,
+            another: null,
+            ingredients: [],
+            feedback: null
         };
+    },
+    methods: {
+        AddSmoothie() {},
+        addIng() {
+            if (this.another) {
+                this.ingredients.push(this.another);
+                this.another = null;
+                this.feedback = null;
+            } else {
+                this.feedback = "You must enter at least an ingredient";
+            }
+        }
     }
 };
 </script>
 
 <style scoped>
+.add-smoothie {
+    margin-top: 60px;
+    padding: 20px;
+    max-width: 500px;
+}
+
+.add-smoothie h2 {
+    font-size: 2em;
+    margin: 20px auto;
+}
+
+.add-smoothie .field {
+    margin: 20px auto;
+}
 </style>
