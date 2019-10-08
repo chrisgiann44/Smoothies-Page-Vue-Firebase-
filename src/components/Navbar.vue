dd42d6dae281f90bd4ac8b485325aa531a053fff
Christos Giannouris <chris1giann1@gmail.com>
2019-10-08 09:38:24 +0200

    first Commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="navbar">
+        <nav class="nav-extented indigo darken-2">
+            <div class="nav-content">
+                <router-link to>
+                    <span class="nav-title">Smoothies</span>
+                </router-link>
+                <a href class="btn-floating btn-large pink">
+                    <router-link to>
+                        <i class="material-icons">add</i>
+                    </router-link>
+                </a>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Navbar",
+    data() {
+        return {};
+    }
+};
+</script>
+
+<style scoped>
+.nav-content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.btn-floating {
+    margin-right: 1em;
+}
+
+.darken-2 {
+    height: 102px;
+}
+.nav-title {
+    padding: 25px;
+}
+</style>
